5b18b9f0cb18d197209c2fe48ffba22ee1c9d518
Smith Winter <hovuminhduc@gmail.com>
2018-12-20 21:02:05 +0700

    Create DataGridView for SudokuTable

--- SudokuGame/SudokuGame/Form1.Designer.cs
+++ SudokuGame/SudokuGame/Form1.Designer.cs
@@ -28,13 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(SudokuMainForm));
             this.SudokuMainMenu = new System.Windows.Forms.MenuStrip();
             this.SudokuNewGame = new System.Windows.Forms.ToolStripMenuItem();
             this.SudokuLoadGame = new System.Windows.Forms.ToolStripMenuItem();
+            this.SudokuValidate = new System.Windows.Forms.ToolStripMenuItem();
+            this.solveToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.SudokuHighScore = new System.Windows.Forms.ToolStripMenuItem();
             this.SudokuHelp = new System.Windows.Forms.ToolStripMenuItem();
             this.SudokuAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.SudokuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.SudokuTable = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column9 = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.SudokuMainMenu.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SudokuTable)).BeginInit();
             this.SuspendLayout();
             // 
             // SudokuMainMenu
@@ -42,8 +59,12 @@
             this.SudokuMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.SudokuNewGame,
             this.SudokuLoadGame,
+            this.SudokuValidate,
+            this.solveToolStripMenuItem,
+            this.SudokuHighScore,
             this.SudokuHelp,
-            this.SudokuAbout});
+            this.SudokuAbout,
+            this.SudokuExit});
             this.SudokuMainMenu.Location = new System.Drawing.Point(0, 0);
             this.SudokuMainMenu.Name = "SudokuMainMenu";
             this.SudokuMainMenu.Size = new System.Drawing.Size(800, 24);
@@ -65,6 +86,26 @@
             this.SudokuLoadGame.Text = "Load Game";
             this.SudokuLoadGame.Click += new System.EventHandler(this.SudokuLoadGame_Click);
             // 
+            // SudokuValidate
+            // 
+            this.SudokuValidate.Name = "SudokuValidate";
+            this.SudokuValidate.Size = new System.Drawing.Size(60, 20);
+            this.SudokuValidate.Text = "Validate";
+            this.SudokuValidate.Click += new System.EventHandler(this.SudokuValidate_Click);
+            // 
+            // solveToolStripMenuItem
+            // 
+            this.solveToolStripMenuItem.Name = "solveToolStripMenuItem";
+            this.solveToolStripMenuItem.Size = new System.Drawing.Size(47, 20);
+            this.solveToolStripMenuItem.Text = "Solve";
+            // 
+            // SudokuHighScore
+            // 
+            this.SudokuHighScore.Name = "SudokuHighScore";
+            this.SudokuHighScore.Size = new System.Drawing.Size(77, 20);
+            this.SudokuHighScore.Text = "High Score";
+            this.SudokuHighScore.Click += new System.EventHandler(this.SudokuHighScore_Click);
+            // 
             // SudokuHelp
             // 
             this.SudokuHelp.Name = "SudokuHelp";
@@ -79,11 +120,82 @@
             this.SudokuAbout.Text = "About";
             this.SudokuAbout.Click += new System.EventHandler(this.SudokuAbout_Click);
             // 
+            // SudokuExit
+            // 
+            this.SudokuExit.Name = "SudokuExit";
+            this.SudokuExit.Size = new System.Drawing.Size(37, 20);
+            this.SudokuExit.Text = "Exit";
+            this.SudokuExit.Click += new System.EventHandler(this.SudokuExit_Click);
+            // 
+            // SudokuTable
+            // 
+            this.SudokuTable.AllowUserToAddRows = false;
+            this.SudokuTable.AllowUserToDeleteRows = false;
+            this.SudokuTable.AllowUserToResizeColumns = false;
+            this.SudokuTable.AllowUserToResizeRows = false;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            this.SudokuTable.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.SudokuTable.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.SudokuTable.ColumnHeadersVisible = false;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.SudokuTable.DefaultCellStyle = dataGridViewCellStyle2;
+            this.SudokuTable.GridColor = System.Drawing.Color.Black;
+            this.SudokuTable.Location = new System.Drawing.Point(408, 58);
+            this.SudokuTable.MultiSelect = false;
+            this.SudokuTable.Name = "SudokuTable";
+            this.SudokuTable.RowHeadersVisible = false;
+            this.SudokuTable.ScrollBars = System.Windows.Forms.ScrollBars.None;
+            this.SudokuTable.Size = new System.Drawing.Size(367, 345);
+            this.SudokuTable.TabIndex = 1;
+            // 
+            // Column1
+            // 
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.Name = "Column2";
+            // 
+            // Column3
+            // 
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.Name = "Column6";
+            // 
+            // Column7
+            // 
+            this.Column7.Name = "Column7";
+            // 
+            // Column8
+            // 
+            this.Column8.Name = "Column8";
+            // 
+            // Column9
+            // 
+            this.Column9.Name = "Column9";
+            // 
             // SudokuMainForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.SudokuTable);
             this.Controls.Add(this.SudokuMainMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
@@ -96,6 +208,7 @@
             this.Load += new System.EventHandler(this.SudokuMainForm_Load);
             this.SudokuMainMenu.ResumeLayout(false);
             this.SudokuMainMenu.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.SudokuTable)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -106,8 +219,22 @@
         private System.Windows.Forms.MenuStrip SudokuMainMenu;
         private System.Windows.Forms.ToolStripMenuItem SudokuNewGame;
         private System.Windows.Forms.ToolStripMenuItem SudokuLoadGame;
+        private System.Windows.Forms.DataGridView SudokuTable;
+        private System.Windows.Forms.ToolStripMenuItem solveToolStripMenuItem;
         private System.Windows.Forms.ToolStripMenuItem SudokuHelp;
         private System.Windows.Forms.ToolStripMenuItem SudokuAbout;
+        private System.Windows.Forms.ToolStripMenuItem SudokuExit;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column9;
+        private System.Windows.Forms.ToolStripMenuItem SudokuValidate;
+        private System.Windows.Forms.ToolStripMenuItem SudokuHighScore;
     }
 }
 
--- SudokuGame/SudokuGame/Form1.cs
+++ SudokuGame/SudokuGame/Form1.cs
@@ -12,6 +12,46 @@ namespace SudokuGame
 {
     public partial class SudokuMainForm : Form
     {
+        public void Build()
+        {
+            SudokuTable.ColumnCount = 9;
+            SudokuTable.Rows.Add(9);                         
+            for (int i = 0; i < 9; i++)
+            {
+                DataGridViewColumn column = SudokuTable.Columns[i];
+                column.Width = (int)(SudokuTable.Width / 9f);
+                DataGridViewRow row = SudokuTable.Rows[i];
+                row.Height = (int)(SudokuTable.Height / 9f);
+            }
+            SudokuTable.Width = SudokuTable.Columns[1].Width * 9;         // Визначення розмірів стовпців та рядків
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    SudokuTable.Rows[i].Cells[j].Style.BackColor = Color.LightCyan;
+                }
+
+            }
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 3; j < 6; j++)
+                {
+                    SudokuTable.Rows[i].Cells[j].Style.BackColor = Color.LightBlue;
+                    SudokuTable.Rows[j].Cells[i].Style.BackColor = Color.LightBlue;
+                }
+
+            }
+
+            for (int i = 3; i < 6; i++)
+            {
+                for (int j = 3; j < 6; j++)
+                {
+                    SudokuTable.Rows[i].Cells[j].Style.BackColor = Color.LightCyan;
+                }
+            }
+        }
         public SudokuMainForm()
         {
             InitializeComponent();
@@ -19,7 +59,7 @@ namespace SudokuGame
 
         private void SudokuMainForm_Load(object sender, EventArgs e)
         {
-
+            Build();
         }
 
 
@@ -47,5 +87,21 @@ namespace SudokuGame
         {
 
         }
+
+        private void SudokuExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void SudokuValidate_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void SudokuHighScore_Click(object sender, EventArgs e)
+        {
+
+        }
+
     }
 }
