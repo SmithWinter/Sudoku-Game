3748c43a8c3c86a342b9add2baa563759c69578f
Smith Winter <hovuminhduc@gmail.com>
2024-05-15 18:43:18 +0700

    Update libraries dependencies

--- SudokuGame/SudokuGame/Properties/Settings.Designer.cs
+++ SudokuGame/SudokuGame/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace SudokuGame.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.9.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -20,18 +20,6 @@ namespace SudokuGame.Properties {
         public static Settings Default {
             get {
                 return defaultInstance;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
-        public string s {
-            get {
-                return ((string)(this["s"]));
-            }
-            set {
-                this["s"] = value;
             }
         }
     }
